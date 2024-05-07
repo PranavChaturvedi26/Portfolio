099bca57e1dc63f09e66e9b28322a6f4e70b0822
Pranav Chaturvedi <chaturvedipranava@gmail.com>
2024-05-07 18:28:20 +0530

    Navbar Links added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -18,7 +18,7 @@ const Hero = () => {
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
             I develop web applications and <br className="sm:block hidden" />{" "}
-            knwoledge of Cybersecurity.
+            learn cyber security.
           </p>
         </div>
       </div>
